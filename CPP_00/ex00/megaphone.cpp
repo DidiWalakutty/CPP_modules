0372d8f66658425bc899ec299d1e429e40f69376
Didi Walakutty <91764214+DidiWalakutty@users.noreply.github.com>
2025-01-16 17:08:17 +0100

    ex00

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/16 16:42:36 by diwalaku      #+#    #+#                 */
+/*   Updated: 2025/01/16 17:04:58 by diwalaku      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = i, i < argc, i++)
+		{
+			
+		}
+	}
+}
