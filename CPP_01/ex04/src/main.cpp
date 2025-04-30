3d5b50d3141ae153845278a4ccf143147523e3b8
Didi Walakuttij <diwalaku@f0r5s4.codam.nl>
2025-04-30 16:02:26 +0200

    update for ex04

--- CPP_01/ex04/src/main.cpp
+++ CPP_01/ex04/src/main.cpp
@@ -6,15 +6,15 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/22 17:18:55 by diwalaku      #+#    #+#                 */
-/*   Updated: 2025/04/22 18:16:51 by diwalaku      ########   odam.nl         */
+/*   Updated: 2025/04/30 16:02:09 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Sed.hpp"
 
-// ifstream reads from files.
-// seekg(0, std::ios::end) moves the pointer to the end of the file.
-// tellg() returns the pointer's position, which is the file size when at the end.
+// Validates the input file and s1.
+// Uses ifstream to try opening the file.
+// seekg moves the read position to end; tellg gets file size.
 static int	check_input(const std::string& filename, const std::string& s1)
 {
 	if (s1.empty())
@@ -39,7 +39,9 @@ static int	check_input(const std::string& filename, const std::string& s1)
 }
 
 // s2 can be empty, we remove every s1 occurence.
-// we need to be able to open filename.
+// ifstream reads from a file, ofstream creates a file for writing.
+// istreambuf_iterator reads every char of infile until EOF
+// and store it in content.
 int	main(int argc, char **argv)
 {
 	if (argc != 4)
@@ -54,11 +56,7 @@ int	main(int argc, char **argv)
 	if (check_input(filename, s1) == 1)
 		return (1);
 
-	// Open the input file to read content
 	std::ifstream infile(filename);
-
-	// Read the entire content of the file into the 'content' string one char at a time.
-	// The istreambuf_iterator creates an iterator at the start end end of the file.
 	std::string content((std::istreambuf_iterator<char>(infile)),
 							std::istreambuf_iterator<char>());
 	infile.close();
