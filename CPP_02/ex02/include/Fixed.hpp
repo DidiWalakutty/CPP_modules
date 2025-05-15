023435873e424241b0bf3d9c80f85e65ce39fab2
Didi Walakutty <didiswalakutty@gmail.com>
2025-05-15 16:23:51 +0200

    do operators

---
+++ CPP_02/ex02/include/Fixed.hpp
@@ -0,0 +1,56 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int _fixedValue;
+		static const int _fractBits = 8;
+
+	public:
+		Fixed();						// Default
+		Fixed(const int intValue);		// Int
+		Fixed(const float floatValue);	// Float
+		Fixed(const Fixed& copy);		// Copy
+		Fixed& operator=(const Fixed& assign); // Assign
+		~Fixed();
+
+		// Comparison - returns true/false
+		bool operator>(const Fixed& other) const;
+		bool operator<(const Fixed& other) const;
+		bool operator>=(const Fixed& other) const;
+		bool operator<=(const Fixed& other) const;
+		bool operator==(const Fixed& other) const;
+		bool operator!=(const Fixed& other) const;
+
+		// Arithmetic - creates new value w/o modifying og
+		Fixed operator+(const Fixed& other) const;
+		Fixed operator-(const Fixed& other) const;
+		Fixed operator*(const Fixed& other) const;
+		Fixed operator/(const Fixed& other) const;
+
+		// Increment/Decrement - pre and post.
+		// Pre - returns a reference to the modified object
+		// Post - returns a copy of the og object, before modification (modifies internally)
+		Fixed& operator++();	// Pre-increment = ++a
+		Fixed& operator--();	// Pre-decrement = --a
+		Fixed operator++(int);	// Post-increment = a++
+		Fixed operator--(int);	// Post-decrement = a--;
+
+		// Static min/max functions - return references to avoid copying.
+		// Provide both modifiable and const versions for flexibility.
+		static Fixed& min(Fixed& a, Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		static const Fixed& min(const Fixed& a, const Fixed& b);
+		static const Fixed& max(const Fixed& a, const Fixed &b);
+
+		float	toFloat() const;
+		int		toInt() const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
---
+++ CPP_02/ex02/src/Fixed.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Fixed.cpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/05/15 15:24:29 by diwalaku      #+#    #+#                 */
+/*   Updated: 2025/05/15 16:15:15 by diwalaku      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Fixed.hpp"
+
+Fixed::Fixed() : _fixedValue(0)
+{	
+	std::cout << "Default constructor called" << std::endl;
+}
+
+// Shift the int left by 8 bits to move it into fixed-point format.
+// This stores the int correctly by reserving the lower 8 bits for the fractional part.
+Fixed::Fixed(const int fixedInt)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixedValue = fixedInt << _fractBits;
+}
+
+// Multiply the float by 256 (1 << 8) to scale it up to fixed-point format
+// and use roundf to choose the closest int value that represents the fixed-point num accurately.
+Fixed::Fixed(const float fixedFloat)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixedValue = roundf(fixedFloat * (1 << _fractBits));
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->_fixedValue = copy._fixedValue;
+}
+
+Fixed& Fixed::operator=(const Fixed& copyAssign)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copyAssign)
+		this->_fixedValue = copyAssign._fixedValue;
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// toFloat(): converts fixed-point back to float by dividing by 256 (1 << _fractBits).
+float	Fixed::toFloat() const
+{
+	return (float)_fixedValue / (1 << _fractBits);
+}
+
+// toInt(): drops the fractional bits by shifting right, returning only the int part.
+int		Fixed::toInt() const
+{
+	return _fixedValue >> _fractBits;
+}
+
+// Tells the compiler how to print a Fixed object with << class Object.
+// Overloads << to print Fixed objects as floats.
+// 'os' holds the current stream (e.g. "a is ") and adds toFloat().
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
+{
+	os << fixed.toFloat();
+	return os;
+}
+
