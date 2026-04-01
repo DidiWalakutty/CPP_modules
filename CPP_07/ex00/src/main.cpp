7074c75d757c7c02afa98c58975a35abde49aab5
diwalaku <diwalaku@student.codam.nl>
2026-04-01 14:19:58 +0200

    updated main

--- CPP_07/ex00/src/main.cpp
+++ CPP_07/ex00/src/main.cpp
@@ -1,11 +1,11 @@
 #include "../include/whatever.hpp"
 #include <iostream>
 
+// :: is the scope resolution operator: use this function from the global scope
 int main( void ) 
 {
 	int a = 2;
 	int b = 3;
-
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
@@ -17,6 +17,13 @@ int main( void )
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
 	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
-	
+
+	char e = '<';
+	char f = '>';
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min(e, f) = " << ::min(e, f) << std::endl;
+	std::cout << "max(e, f) = " << ::max(e, f) << std::endl;
+
 	return 0;
 }
