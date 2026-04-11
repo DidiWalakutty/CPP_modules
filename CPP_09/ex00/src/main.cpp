9a833f0facea0c0154c94cd6544349a0e293194b
diwalaku <diwalaku@student.codam.nl>
2026-04-11 17:51:43 +0200

    Add basic argument handling in main function
    - Implemented argument count check to ensure correct usage of the program.
    - Added error message output for incorrect usage.

--- CPP_09/ex00/src/main.cpp
+++ CPP_09/ex00/src/main.cpp
@@ -6,8 +6,19 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/10 18:07:59 by diwalaku      #+#    #+#                 */
-/*   Updated: 2026/04/10 18:08:17 by diwalaku      ########   odam.nl         */
+/*   Updated: 2026/04/11 17:46:49 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		std::cout << RED << "Error: Correct Usage: ./btc <input_file>" << RESET << std::endl;
+		return 1;
+	}
+	
+}
