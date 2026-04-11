65119bb11330b7da275eaae16d6ccc088ede8f8e
diwalaku <diwalaku@student.codam.nl>
2026-04-11 17:51:31 +0200

    Refactor BitcoinExchange class definition and add necessary includes

--- CPP_09/ex00/include/BitcoinExchange.hpp
+++ CPP_09/ex00/include/BitcoinExchange.hpp
@@ -1,2 +1,18 @@
 #pragma once
 
+#include <iostream>
+
+#define RESET   "\033[0m"
+#define MAGENTA "\033[35m"
+#define RED	 	"\033[31m"
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &copy);
+		BitcoinExchange &operator=(const BitcoinExchange &assign);
+		~BitcoinExchange();
+
+		
+};
