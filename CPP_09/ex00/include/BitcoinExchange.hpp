cc115e776aedde3c587cf862372efaa2e7024eca
Didi Walakutty <didiswalakutty@gmail.com>
2026-04-14 01:29:30 +0200

    Refactor BitcoinExchange class: add private members and method declarations

--- CPP_09/ex00/include/BitcoinExchange.hpp
+++ CPP_09/ex00/include/BitcoinExchange.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <map>
 
 #define RESET   "\033[0m"
 #define MAGENTA "\033[35m"
@@ -8,11 +10,26 @@
 
 class BitcoinExchange
 {
+	private:
+		std::map<std::string, double> _exchangeRates;	// date -> rate, automatically sorted by key (YYYY-MM-DD)
+		
+		void loadCSV(const std::string& dataFile);
+		bool isValidDate(const std::string& date) const;
+		double getExchangeRateForDate(const std::string& date) const;
+		
+		// helper functions
+		std::string trim(const std::string& str) const;
+		bool isLeapYear(int year) const;
+		int getDaysInMonth(int month, int year) const;
+
 	public:
-		BitcoinExchange();
+		BitcoinExchange() = delete; // do we want to delete
+		BitcoinExchange(const std::string &dataFile);
 		BitcoinExchange(const BitcoinExchange &copy);
 		BitcoinExchange &operator=(const BitcoinExchange &assign);
 		~BitcoinExchange();
+
+		void processInputFile(const std::string &inputFile);
 
 		
 };
