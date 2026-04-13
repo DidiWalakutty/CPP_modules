280f997746b06d40a851a507e2bdcab15cc84d9b
Didi Walakutty <didiswalakutty@gmail.com>
2026-04-14 01:30:02 +0200

    Fix include path for BitcoinExchange and improve error handling in main function

--- CPP_09/ex00/src/main.cpp
+++ CPP_09/ex00/src/main.cpp
@@ -6,18 +6,29 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/10 18:07:59 by diwalaku      #+#    #+#                 */
-/*   Updated: 2026/04/11 17:46:49 by diwalaku      ########   odam.nl         */
+/*   Updated: 2026/04/13 21:55:16 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
+#include "../include/BitcoinExchange.hpp"
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
-	{
+{
 		std::cout << RED << "Error: Correct Usage: ./btc <input_file>" << RESET << std::endl;
+		return 1;
+	}
+	
+	try
+	{
+		BitcoinExchange btcExchange("data/data.csv");
+		btcExchange.processInputFile(argv[1]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << RED << "Error: " << e.what() << RESET << std::endl;
 		return 1;
 	}
 	
