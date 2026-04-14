c6aaa267f6561543c6cd3246758579a11c171e77
diwalaku <diwalaku@student.codam.nl>
2026-04-14 16:48:24 +0200

    Commented out year validation in isValidDate method

--- CPP_09/ex00/src/Utils.cpp
+++ CPP_09/ex00/src/Utils.cpp
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/13 21:59:29 by diwalaku      #+#    #+#                 */
-/*   Updated: 2026/04/14 00:53:53 by diwalaku      ########   odam.nl         */
+/*   Updated: 2026/04/14 16:39:36 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,8 @@ bool BitcoinExchange::isValidDate(const std::string& date) const
 		return false;
 	}
 
+	// if (year < 2009 || year > 2026)
+	// 	return false;
 	if (month < 1 || month > 12)
 		return false;
 	
