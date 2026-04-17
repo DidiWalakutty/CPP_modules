bcfd4af283668ef38ea912bf051c514aba5eef9b
Didi Walakutty <didiswalakutty@gmail.com>
2026-04-18 00:37:50 +0200

    Refactor main function in main.cpp for improved error handling and usage message

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -6,8 +6,30 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/14 20:05:36 by diwalaku      #+#    #+#                 */
-/*   Updated: 2026/04/14 20:05:53 by diwalaku      ########   odam.nl         */
+/*   Updated: 2026/04/17 21:51:34 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PmergeMe.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Usage: ./PmergeMe <numbers...>" << std::endl;
+		return 1;
+	}
+	try
+	{
+		PmergeMe FJSorter(argv);
+		FJSorter.run();
+		// FJSorter.printResults();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+		return 1;
+	}
+
+	return 0;
+}
