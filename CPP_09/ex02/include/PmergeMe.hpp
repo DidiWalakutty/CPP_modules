1d1cb457c960fa78d0544b3ed07c3a7ef2bf46db
diwalaku <diwalaku@student.codam.nl>
2026-04-22 22:50:44 +0200

    Refactor printContainer function and rename insertion methods for clarity

--- CPP_09/ex02/include/PmergeMe.hpp
+++ CPP_09/ex02/include/PmergeMe.hpp
@@ -3,18 +3,30 @@
 #include <iostream>
 #include <vector>
 #include <deque>
-#include <ctime>
 #include <chrono>
 #include <algorithm>
 
 template <typename T>
 void printContainer(const T& container)
 {
-	for (typename T::const_iterator it = container.begin(); it != container.end(); ++it)
+	size_t size = container.size();
+	if (size <= 15)
 	{
-		std::cout << *it << " ";
+		for (typename T::const_iterator it = container.begin(); it != container.end(); ++it)
+		{
+			std::cout << *it << " ";
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
+	else
+	{
+		for (size_t i = 0; i < 15; i++)
+		{
+			std::cout << container[i] << " ";
+		}
+		std::cout << "[...] ";
+		std::cout << std::endl;
+	}
 }
 
 class PmergeMe
@@ -29,8 +41,8 @@ class PmergeMe
 		void FJSortDeque(std::deque<int> &deq);
 
 		// Insert Small Elements
-		void FJMergeInsertVector(std::vector<int> &mainChain, const std::vector<int> &pendChain);
-		void FJMergeInsertDeque(std::deque<int> &mainChain, const std::deque<int > &pendChain);
+		void insertSmallVecChain(std::vector<int> &mainChain, const std::vector<int> &pendChain);
+		void insertSmallDeqChain(std::deque<int> &mainChain, const std::deque<int> &pendChain);
 
 		bool validateInput(char **argv);
 		void parseInput(char **argv);
