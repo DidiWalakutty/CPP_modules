d3dc43aa4cf0cae32251ff991300af9041a9f1f3
diwalaku <diwalaku@student.codam.nl>
2026-04-24 21:17:07 +0200

    Update file header and enhance documentation for Ford Johnson sort phases

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/14 20:05:36 by diwalaku      #+#    #+#                 */
-/*   Updated: 2026/04/18 02:04:31 by diwalaku      ########   odam.nl         */
+/*   Updated: 2026/04/24 19:54:46 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,15 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
+/**
+ * @brief Ford Johnson (merge insert) sort has 3 phases
+ * 
+ * 1. Pairing
+ *    Group elements into pairs and normalize them: (a, b) -> (min(a, b), max(a, b))
+ * 2. Recursive Sorting
+ *    Sort the pairs by their larger elements.
+ * 3. Controlled Insertion
+ *    Insert the smaller elements into the sorted chain, through Jacobsthal sequence, to minimize comparisons. 
+ * 
+ */
