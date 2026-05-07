a721959cdecda8c20a919ac30c0ec1ce440257fd
diwalaku <diwalaku@student.codam.nl>
2026-05-07 21:10:36 +0200

    Update main.cpp: revise update timestamp, remove duplicate error management comment, and clean up unnecessary documentation

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/14 20:05:36 by diwalaku      #+#    #+#                 */
-/*   Updated: 2026/04/24 19:54:46 by diwalaku      ########   odam.nl         */
+/*   Updated: 2026/05/07 16:59:54 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PmergeMe.hpp"
+
 
 int main(int argc, char **argv)
 {
@@ -21,9 +22,6 @@ int main(int argc, char **argv)
 	}
 	try
 	{
-		
-		/// !!! duplicates: The management of errors related to duplicates is left to your discretion.
-
 		PmergeMe FJSorter(argv);
 		FJSorter.run();
 	}
@@ -32,18 +30,6 @@ int main(int argc, char **argv)
 		std::cerr << "Error: " << e.what() << '\n';
 		return 1;
 	}
-
+	
 	return 0;
 }
-
-/**
- * @brief Ford Johnson (merge insert) sort has 3 phases
- * 
- * 1. Pairing
- *    Group elements into pairs and normalize them: (a, b) -> (min(a, b), max(a, b))
- * 2. Recursive Sorting
- *    Sort the pairs by their larger elements.
- * 3. Controlled Insertion
- *    Insert the smaller elements into the sorted chain, through Jacobsthal sequence, to minimize comparisons. 
- * 
- */
